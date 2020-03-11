e091d532187ef3a1de4430e25f21bec831580d33
Jack Bayliss <jack@jackbayliss.com>
2020-03-11 22:32:53 +0000

    Tests
    Initial creation of tests, for CI.
    
    Will need to add more tests potential.

---
+++ tests/test_missioncheifbot.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('../botfiles')
+import missionchief_bot
+
+def test_correct_login():
+    assert missionchief_bot.login('biguktest@live.com','lol123') == True
+    missionchief_bot.browser.close()
