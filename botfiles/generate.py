b5d143e427e2a1295478f9015caa0165326d9c96
Jack Bayliss <jack@jackbayliss.com>
2020-04-01 00:48:56 +0100

    Update generate.py
    Fix generate.py to prevent errors.

--- botfiles/generate.py
+++ botfiles/generate.py
@@ -36,9 +36,9 @@ def getRequirements():
       if "Required" in r.text or "Wymagane" in r.text or "Wymagany" in r.text or "Требуемые" in r.text or "Benodigde" in r.text or "benodigd" in r.text or "Nödvändiga" in r.text or "richieste" in r.text or "richiesta" in r.text or "richiesti" in r.text or "Benötigte" in r.text:
        if "Station" not in r.text and "posterunki" not in r.text and "Caserme" not in r.text and "Stazioni" not in r.text and "Possibilità" not in r.text and "Possibile" not in r.text and "brandstationer" not in r.text and "räddningsstationer" not in r.text:
         requirement = r.text.replace('Required','').replace('Wymagane','').replace('Wymagany','').replace('Требуемые','').replace("Benodigde",'').replace("benodigd",'').replace("Nödvändiga","").replace("richieste","").replace("richiesti","").replace("richiesta","").replace("Benötigte","").strip().lower()
-       qty = requirements[index+1].text
-       print(f"Requirement found :   {str(qty)} x {str(requirement)}")
-       requiredlist.append({'requirement':requirement,'qty': qty })
+        qty = requirements[index+1].text
+        print(f"Requirement found :   {str(qty)} x {str(requirement)}")
+        requiredlist.append({'requirement':requirement,'qty': qty })
   if(len(requiredlist)==0):
    requiredlist.append({'requirement':'ambulance','qty': 1 })
   return requiredlist
