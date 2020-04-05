1e7742d2a762b0a49c2cd7c0f5d20aced780ebd4
Jack Bayliss <jack@jackbayliss.com>
2020-04-05 13:43:11 +0100

    Update test_missionchiefbot.py

--- tests/test_missionchiefbot.py
+++ tests/test_missionchiefbot.py
@@ -3,5 +3,5 @@ sys.path.append('../botfiles')
 import missionchief_bot
 
 def test_correct_login():
-    assert missionchief_bot.login('biguktest@live.com','lol123') == True
+    assert missionchief_bot.login('biguktest@live.com','lol123',missionchief_bot.browser) == True
     missionchief_bot.browser.close()
