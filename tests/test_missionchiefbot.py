d68fb6938d044361539feaa8d777280a992a4550
Jack <jack@jackbayliss.com>
2020-04-13 23:40:08 +0100

    Update test_missionchiefbot.py

--- tests/test_missionchiefbot.py
+++ tests/test_missionchiefbot.py
@@ -3,5 +3,5 @@ sys.path.append('../botfiles')
 import missionchief_bot
 
 def test_correct_login():
-    assert missionchief_bot.login('biguktest@live.com','lol123',missionchief_bot.browser) == True
+    assert missionchief_bot.login('hellouk133334','lol123',missionchief_bot.browser) == True
     missionchief_bot.browser.close()
